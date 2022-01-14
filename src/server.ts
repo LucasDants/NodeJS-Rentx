9760fb06baed7d3f347b01d0cb4b86ec8ad4311c
Lucas Dantas <lucasdantasmoura01@gmail.com>
2022-01-14 18:47:47 -0300

    Refatoração das rotas

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,7 @@
 import express from "express";
 import swaggerUi from "swagger-ui-express";
 
-import { categoriesRoutes } from "./routes/categories.routes";
-import { specificationsRoutes } from "./routes/specifications.routes";
+import { router } from "./routes";
 import swaggerFile from "./swagger.json";
 
 const app = express();
@@ -11,7 +10,6 @@ app.use(express.json());
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
-app.use("/categories", categoriesRoutes);
-app.use("/specifications", specificationsRoutes);
+app.use(router);
 
 app.listen(3333);
