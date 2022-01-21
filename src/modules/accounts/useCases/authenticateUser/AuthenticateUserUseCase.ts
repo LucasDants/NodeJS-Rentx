1313bbf78c8a9680c1313afb330d476f8c9e7cf5
Lucas Dantas <lucasdantasmoura01@gmail.com>
2022-01-21 16:01:10 -0300

    auth de usuários

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,54 @@
+import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
+import { inject, injectable } from "tsyringe";
+
+import { IUsersRepository } from "../../repositories/IUserRepository";
+
+interface IRequest {
+    email: string;
+    password: string;
+}
+
+interface IResponse {
+    user: {
+        name: string;
+        email: string;
+    };
+    token: string;
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+    constructor(
+        @inject("UsersRepository")
+        private usersRepository: IUsersRepository
+    ) {}
+    async execute({ email, password }: IRequest): Promise<IResponse> {
+        const user = await this.usersRepository.findByEmail(email);
+
+        if (!user) {
+            throw new Error("Email or password incorrect!");
+        }
+
+        const passwordMatch = await compare(password, user.password);
+
+        if (!passwordMatch) {
+            throw new Error("Email or password incorrect!");
+        }
+
+        const token = sign({}, "57025fa59e482d591353708437f11bc2", {
+            subject: user.id,
+            expiresIn: "1d",
+        });
+
+        return {
+            user: {
+                name: user.name,
+                email: user.email,
+            },
+            token,
+        };
+    }
+}
+
+export { AuthenticateUserUseCase };
