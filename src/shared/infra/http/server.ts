e598ea1a0434048ee1f92c14dc9d3f6b88e7c3e1
Lucas Dantas <lucasdantasmoura01@gmail.com>
2022-01-29 16:37:48 -0300

    seed

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -2,14 +2,15 @@ import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
 
-import "../typeorm";
-
 import "@shared/container";
 
 import { AppError } from "@shared/errors/AppError";
 
 import swaggerFile from "../../../swagger.json";
+import createConnection from "../typeorm";
 import { router } from "./routes";
+
+createConnection();
 
 const app = express();
 
