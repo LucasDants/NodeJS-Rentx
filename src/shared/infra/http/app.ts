031b6f514d53e4a3ab389d95c71f0b443f317dad
Lucas Dantas <lucasdantasmoura01@gmail.com>
2022-01-30 18:20:40 -0300

    Teste de integração

---
+++ src/shared/infra/http/app.ts
@@ -0,0 +1,37 @@
+import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors";
+import "reflect-metadata";
+import swaggerUi from "swagger-ui-express";
+
+import "@shared/container";
+
+import { AppError } from "@shared/errors/AppError";
+
+import swaggerFile from "../../../swagger.json";
+import createConnection from "../typeorm";
+import { router } from "./routes";
+
+createConnection();
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
+app.use(router);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+        return res.status(err.statusCode).json({
+            message: err.message,
+        });
+    }
+
+    return res.status(500).json({
+        status: "error",
+        message: err.message,
+    });
+});
+
+export { app };
