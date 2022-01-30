e453217bf994f2a0395f13fc1cf97e7d6312bfbe
Lucas Dantas <lucasdantasmoura01@gmail.com>
2022-01-30 15:27:04 -0300

    TDD cadastro de aluguel

---
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
@@ -0,0 +1,42 @@
+import { Rental } from "@modules/rentals/infra/typeorm/entitites/Rental";
+import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
+import { AppError } from "@shared/errors/AppError";
+
+interface IRequest {
+    user_id: string;
+    car_id: string;
+    expected_return_date: Date;
+}
+
+class CreateRentalUseCase {
+    constructor(private rentalsRepository: IRentalsRepository) {}
+
+    async execute({
+        user_id,
+        car_id,
+        expected_return_date,
+    }: IRequest): Promise<Rental> {
+        const carNotAvailable =
+            await this.rentalsRepository.findOpenRentalByCar(car_id);
+
+        if (carNotAvailable) {
+            throw new AppError("Car is not available");
+        }
+
+        const rentalOpenToUser =
+            await this.rentalsRepository.findOpenRentalByUser(user_id);
+
+        if (rentalOpenToUser) {
+            throw new AppError("There is a rental in progress for user!");
+        }
+
+        const rental = await this.rentalsRepository.create({
+            user_id,
+            car_id,
+            expected_return_date,
+        });
+        return rental;
+    }
+}
+
+export { CreateRentalUseCase };
