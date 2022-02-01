dd62e702d52b9e3460354aa8b9aef3e5dab0b691
Lucas Dantas <lucasdantasmoura01@gmail.com>
2022-02-01 15:31:41 -0300

    Upload de imagens no S3 e configuração do upload em desenvolvimento

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -2,18 +2,17 @@ import crypto from "crypto";
 import multer from "multer";
 import { resolve } from "path";
 
-export default {
-    upload(folder: string) {
-        return {
-            storage: multer.diskStorage({
-                destination: resolve(__dirname, "..", "..", folder),
-                filename: (req, file, callback) => {
-                    const fileHash = crypto.randomBytes(16).toString("hex");
+const tmpFolder = resolve(__dirname, "..", "..", "tmp");
 
-                    const filename = `${fileHash}-${file.originalname}`;
-                    return callback(null, filename);
-                },
-            }),
-        };
-    },
+export default {
+    tmpFolder,
+    storage: multer.diskStorage({
+        destination: tmpFolder,
+        filename: (req, file, callback) => {
+            const fileHash = crypto.randomBytes(16).toString("hex");
+
+            const filename = `${fileHash}-${file.originalname}`;
+            return callback(null, filename);
+        },
+    }),
 };
--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,10 +1,12 @@
 import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
 import "reflect-metadata";
+import "dotenv/config";
 import swaggerUi from "swagger-ui-express";
 
 import "@shared/container";
 
+import upload from "@config/upload";
 import { AppError } from "@shared/errors/AppError";
 
 import swaggerFile from "../../../swagger.json";
@@ -18,6 +20,9 @@ const app = express();
 app.use(express.json());
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
+app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
+app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 
 app.use(router);
 
